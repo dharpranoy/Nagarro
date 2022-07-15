8bf13bacd4ae46503b1164078b88f8c3b2635013
localp <tutor.pnd19@gmail.com>
2022-07-15 18:24:30 +0530

    fixed bug

--- popup/script.js
+++ popup/script.js
@@ -1,33 +1,46 @@
-$(document).ready(()=>{
+let valid=true
+let mailv=true
+$(document).ready(()=>{	
 	$('.pop').click(()=>{
+		$('#name').text('')
+		$('#mail').text('')
 		$('#box').addClass('show')
 	})
 
 	$('#close').click(()=>{
 		$('#box').removeClass('show')		
 	})
-	$('#submission').click(()=>{
-		let valid=false
-		let name=document.getElementById('uname').value
-		if (name.length>3){
-			valid=valid&true
-			$('#warn-name').text('')
+	$('#uname').keyup(()=>{
+		let name=$('#uname').val()
+		$('#warn-name').text('')
+		if (name.length>=3){
+			valid=true
 		}else{
+			console.log('here')
+			valid=false
+			$('#warn-name').text('Invalid username')
+		}
+		console.log(valid)
+	})
+
+	$('#mail').keyup(()=>{
+		let mail=$('#mail').val()
+		$('#warn-mail').text('')
+		let regex=/^([\-\.0-9a-zA-Z]+)@([\-\.0-9a-zA-Z]+)\.([a-zA-Z]){2,7}$/
+		if (regex.test(mail)){
 			
-			$('warn-name').text('Invalid username')
-		}
-		let mail=document.getElementById('mail').value
-		let regex='/^([\-\.0-9a-zA-Z]+)@([\-\.0-9a-zA-Z]+)\.([a-zA-Z]){2,7}$/'
-		if (mail.test(regex)){
-			valid=valid&true
-			$('#warn-mail').text('')
+			mailv=true
 		}else{
-		
-			$('warn-mail').text('Invalid email')
+			mailv=false
+			$('#warn-mail').text('Invalid email')
 		}
-		if (valid==true){
-			$('warn-name').text('')
-			$('warn-mail').text('')
+		console.log(valid)
+	})
+	$('#submission').click(()=>{
+		console.log(valid)
+		if (valid==true && mailv==true){
+			$('#uname').text('')
+			$('#mail').text('')
 			$('#box').removeClass('show')
 		}
 	})

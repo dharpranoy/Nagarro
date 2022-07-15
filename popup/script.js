d499ddd04aeaf47f886c85f8d81c057127ecc0a2
dharpranoy <76781789+dharpranoy@users.noreply.github.com>
2022-07-15 18:28:47 +0530

    Update script.js

--- popup/script.js
+++ popup/script.js
@@ -2,9 +2,7 @@
 let valid=true
 let mailv=true
 $(document).ready(()=>{
-	let valid=false
 	$('.pop').click(()=>{
-		valid=false
 		$('#name').text('')
 		$('#mail').text('')
 		$('#box').addClass('show')
@@ -40,33 +38,10 @@ $(document).ready(()=>{
 		}
 		console.log(valid)
 	})
+	
+	
 	$('#submission').click(()=>{
-		console.log(valid)
 		if (valid==true && mailv==true){
-
-	$('#mail').keyup(()=>{
-		let mail=$('#mail').val()
-		let regex=/^([\-\.0-9a-zA-Z]+)@([\-\.0-9a-zA-Z]+)\.([a-zA-Z]){2,7}$/
-		if (regex.test(mail)){
-			valid=valid&true
-			$('#warn-mail').text('')
-		}else{
-			valid=valid&false
-			$('warn-mail').text('Invalid email')
-		}
-	}
-	$('#uname').keyup(()=>{
-		let name=$('#uname').val()
-		if (name.length>=3){
-			valid=valid&true
-			$('#warn-name').text('')
-		}else{
-			valid=valid&false
-			$('warn-name').text('Invalid username')
-		}
-	})
-	$('#submission').click(()=>{
-		if (valid==true){
 			$('#uname').text('')
 			$('#mail').text('')
 			$('#box').removeClass('show')

730c36fa5ab6290e5a61357730edc9a4b3b7c1f3
localp <tutor.pnd19@gmail.com>
2022-07-25 17:46:20 +0530

    added theme

---
+++ theme-changer/script.js
@@ -0,0 +1,9 @@
+listen=(color,outline)=>{
+	let par=document.getElementById('para')
+	par.style.background=color
+	par.style.borderColor=outline
+	par.style.color=outline
+	let doc=document.getElementById('cnt')
+	doc.style.background=color
+}
+

e9bd2a29e6ef386c2e2f6242ec25c3a152c1c9db
Pranoy Dhar <tutor.pnd19@gmail.com>
2022-07-29 21:21:16 +0530

    added login with passport google github and local to twitter frontpage

--- Twitter/app.js
+++ Twitter/app.js
@@ -4,8 +4,10 @@ const path = require('path')
 const bodyParser = require('body-parser')
 const multer = require('multer')()
 const session = require('express-session')
+const passport = require('passport')
 const bcrypt = require('bcrypt')
 const ensureLogin = require('connect-ensure-login')
+require('./auth.js')
 app.set('views','views')
 app.set('view engine','ejs')
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,6 +25,45 @@ passport.serializeUser((user,done)=>{
 passport.deserializeUser((user,done)=>{
 	done(null,user)
 })
-app.listen(5400,()=>{
-	console.log('server started at 5400')
+app.get('/login',(req,res)=>{
+	res.render('login')
+})
+app.post('/login/password',passport.authenticate('local',{failureRedirect:'/login'}),(req,res)=>{
+	res.render('logout')
+})
+app.post('/login/register',(req,res)=>{
+	let name = req.body.username
+	let mail = req.body.email
+	let password = req.body.password
+	bcrypt.hash(password,10)
+	.then(hashed=>{
+		conn.query(`INSERT INTO auth VALUES ('${name}','${hashed}','${mail}')`,(err,result)=>{
+			if (err) throw err
+			res.redirect('/login/password')
+		})
+	})
+
+})
+app.get('/auth/google',
+  passport.authenticate('google', { scope:
+      [ 'email', 'profile' ] }
+));
+
+app.get('/auth/google/callback',
+    passport.authenticate( 'google', {
+        successRedirect: '/dashboard',
+        failureRedirect: '/'
+}));
+app.get('/dashboard',ensureLogin.ensureLoggedIn(),(req,res)=>{
+	console.log(req.user.displayName)
+	res.render('secret',{
+		name:req.user.displayName
+	})
+})
+app.get('/logout',ensureLogin.ensureLoggedIn(),(req,res)=>{
+		req.logout()
+		res.redirect('/')
+})
+app.listen(8080,()=>{
+	console.log('server started at 8080')
 })

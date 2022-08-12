486c0ab857c7ccc3853bf69c023c892ddb151e4e
localp <tutor.pnd19@gmail.com>
2022-08-12 11:23:36 +0530

    mongodb crud

--- nodejs/mongodb/server.js
+++ nodejs/mongodb/server.js
@@ -1,17 +1,31 @@
 const express = require('express')
 const app = express()
+const bodyparser = require('body-parser')
+const multer = require('multer')()
+const session = require('express-session')
 const mongo = require('mongodb').MongoClient
 let URL = 'mongodb://localhost:27017/'
-mongo.connect(URL,(err,conn)=>{
+let db = null
+mongo.connect(URL,async(err,conn)=>{
   if (err) throw err
   console.log('connected')
-  let db=conn.db('cred')
-  let token = {}
-  db.collection('movies').find(token).toArray((error,result)=>{
-    if (error) throw error
-    console.log(result)
-    conn.close()
-  })
+  db=conn.db('lambda')
+})
+app.use(bodyparser.urlencoded({extended:true}))
+app.get('/retrieve',(req,res)=>{
+	let token = {}
+	db.collection('comments').find(token).toArray((error,result)=>{
+    	if (error) throw error
+    	console.log(result)
+		res.send(result)
+     })
+
+})
+app.post('/notes',(req,res)=>{
+	const notes = { text:req.body.txt, title:req.body.title }
+	db.collection('comments').insert(notes,(err,result)=>{
+		res.send(result)
+	})
 })
 app.listen(4100)
 
